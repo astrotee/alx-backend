89d839173c1486ecb65ef42834c4b545d1637a7c
amin ahmed <aminahmed24@gmail.com>
2024-10-31 07:27:10 +0300

    0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"Basic dictionary"
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self) -> None:
+        "init base class"
+        super().__init__()
+
+    def put(self, key, item):
+        "cache a key-value"
+        if None not in (key, item):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        "get a cached value by key"
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
